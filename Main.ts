944415fdfbbfcced6d5b53d9ab7c3d0617433a88
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-12-12 02:50:26 +0200

    Working on Changing Parser for TypeScript

--- Main.ts
+++ Main.ts
@@ -1,7 +1,14 @@
+import * as dotenv from "dotenv";
 import Lexing from "./Lexing";
+import Parser from "./Parser";
+dotenv.config();
 
 function main() {
-  let lexing = new Lexing("./SourceCode/Test.py");
+  let lexing = new Lexing(process.env.FILE_IN);
+  lexing.defineTokens();
+
+  let parser = new Parser(lexing.getTokens());
+  parser.start();
 }
 
 main();
--- SourceCode/Test.py
+++ SourceCode/Test.py
@@ -1,4 +1,6 @@
+# Test
+
 def test():
   b = 1
 
-a = 5
+a = 5.5
