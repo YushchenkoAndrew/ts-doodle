579df9a3a565a99c0af21477227f486a1c900620
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-12-19 03:06:53 +0200

    Finish on transforming Statement's basic functionality

--- Parser/Statement/Statement.ts
+++ Parser/Statement/Statement.ts
@@ -2,7 +2,9 @@ import ErrorHandler from "../../Error/Error";
 import Parser from "../Parser";
 import Expression from "../Expression/Expression";
 import { Declaration, Operation } from "../Interfaces";
-import { Assign, Condition, ForLoop, FuncCall } from "./Interfaces";
+import { Assign, Condition, ForLoop, FuncCall, Return } from "./Interfaces";
+import { getDefinedToken, isInclude } from "../../lib/";
+import { List, Types } from "../Expression/Interfaces";
 
 class Statement {
   err: ErrorHandler;
@@ -45,68 +47,77 @@ class Statement {
     return { type: "FUNC", name: `_${value}`, params: params, body: [], defined: { value: "", type: "ANY" } };
   }
 
-  // function parseIf() {
-  //   // Delete all spaces
-  //   this.tokens[this.line].push(...this.tokens[this.line].splice(this.index).filter((token) => token.type != "Space"));
-  //   this.stateChecker("type", this.tokens[this.line][this.index], "Wrong If Statement declaration", "Variable", "Number", "String", "Unary", "Parentheses");
-  //   let exp = this.parseExpression({});
-  //   this.stateChecker("type", this.tokens[this.line][this.index++], "Wrong If Statement declaration", "Start Block");
-  //   return { type: "IF", Expression: exp, defined: this.type.curr };
-  // }
+  parseIf(ptr: Parser): Condition {
+    // Delete all spaces
+    ptr.tokens[ptr.line].push(...ptr.tokens[ptr.line].splice(ptr.index).filter((token) => token.type != "Space"));
+    this.err.checkObj(
+      "type",
+      ptr.tokens[ptr.line][ptr.index],
+      { name: "SyntaxError", message: "Wrong If Statement declaration", ptr },
+      "Variable",
+      "Number",
+      "String",
+      "Unary",
+      "Parentheses"
+    );
+    let exp = this.exp.parse(ptr);
+    this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Wrong If Statement declaration", ptr }, "Start Block");
+    return { type: "IF", Expression: exp, body: [] as Operation[] };
+  }
 
-  // // Continue of method Parse If but here we can handle the else and else-if statement
-  // function parseElse(level, body) {
-  //   let { type } = this.tokens[this.line][this.index];
+  // Continue of method Parse If but here we can handle the else and else-if statement
+  parseElse(ptr: Parser, level: number, body: Operation[]) {
+    let { type } = ptr.tokens[ptr.line][ptr.index];
 
-  //   // Small parser for Else Statement and else if statement
-  //   // In a switch I just get an array of elements without the last one
-  //   switch (type.split(/\ /).slice(0, -1).join("-")) {
-  //     case "ELSE": {
-  //       // Delete all spaces
-  //       this.tokens[this.line].push(...this.tokens[this.line].splice(this.index++).filter((token) => token.type != "Space"));
-  //       this.stateChecker("type", this.tokens[this.line][this.index++], "Wrong ELSE Statement declaration", "Start Block");
+    // Small parser for Else Statement and else if statement
+    // In a switch I just get an array of elements without the last one
+    switch (type.split(/\ /).slice(0, -1).join("-")) {
+      case "ELSE": {
+        // Delete all spaces
+        ptr.tokens[ptr.line].push(...ptr.tokens[ptr.line].splice(ptr.index++).filter((token) => token.type != "Space"));
+        this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Wrong ELSE Statement declaration", ptr }, "Start Block");
 
-  //       // Save if body for using it as a header after that
-  //       let header = JSON.parse(JSON.stringify(this.currLevel.body));
+        // Save if body for using it as a header after that
+        let header = JSON.parse(JSON.stringify(ptr.currLevel.body));
 
-  //       // Create a new (Empty body)
-  //       this.currLevel.header.push(...body.slice(0, -1));
-  //       this.currLevel.body = [];
+        // Create a new (Empty body)
+        ptr.currLevel.header.push(...body.slice(0, -1));
+        ptr.currLevel.body = [];
 
-  //       level = this.initStateMachine(level + 1, true);
-  //       body.slice(-1)[0].Statement.else = this.currLevel.body;
-  //       this.currLevel.header.push(...header);
-  //       break;
-  //     }
+        level = ptr.initStateMachine(level + 1, true);
+        (body.slice(-1)[0].Statement as Condition).else = ptr.currLevel.body;
+        ptr.currLevel.header.push(...header);
+        break;
+      }
 
-  //     case "ELSE-IF": {
-  //       // Change "ELSE IF" token to the ELSE-IF for calling the if parser
-  //       // Because it should be a similar one
-  //       this.tokens[this.line][this.index].type = "ELSE-IF Keyword";
+      case "ELSE-IF": {
+        // Change "ELSE IF" token to the ELSE-IF for calling the if parser
+        // Because it should be a similar one
+        ptr.tokens[ptr.line][ptr.index].type = "ELSE-IF Keyword";
 
-  //       // Go down to the previous level, that needs because
-  //       // "else if" statement doesn't create a "Depth Tree"
-  //       this.currLevel.level--;
+        // Go down to the previous level, that needs because
+        // "else if" statement doesn't create a "Depth Tree"
+        ptr.currLevel.level--;
 
-  //       // Save if body for using it as a header after that
-  //       let header = JSON.parse(JSON.stringify(this.currLevel.body));
+        // Save if body for using it as a header after that
+        let header = JSON.parse(JSON.stringify(ptr.currLevel.body));
 
-  //       // Create a new (Empty body)
-  //       this.currLevel.header.push(...body.slice(0, -1));
-  //       this.currLevel.body = [];
+        // Create a new (Empty body)
+        ptr.currLevel.header.push(...body.slice(0, -1));
+        ptr.currLevel.body = [];
 
-  //       level = this.initStateMachine(level, true);
-  //       body.slice(-1)[0].Statement.else = this.currLevel.body;
+        level = ptr.initStateMachine(level, true);
+        (body.slice(-1)[0].Statement as Condition).else = ptr.currLevel.body;
 
-  //       // Restore level
-  //       this.currLevel.level++;
-  //       this.currLevel.header.push(...header);
-  //       break;
-  //     }
-  //   }
+        // Restore level
+        ptr.currLevel.level++;
+        ptr.currLevel.header.push(...header);
+        break;
+      }
+    }
 
-  //   return level;
-  // }
+    return level;
+  }
 
   parseWhile(ptr: Parser): Condition {
     // Delete all spaces
@@ -127,40 +138,39 @@ class Statement {
     return { type: "WHILE", Expression: exp, body: [] as Operation[] };
   }
 
-  // function parseFor() {
-  //   // Delete all spaces
-  //   this.tokens[this.line].push(...this.tokens[this.line].splice(this.index).filter((token) => token.type != "Space"));
-  //   this.stateChecker("type", this.tokens[this.line][this.index], "Wrong For loop declaration", "Variable");
+  parseFor(ptr: Parser): ForLoop {
+    // Delete all spaces
+    ptr.tokens[ptr.line].push(...ptr.tokens[ptr.line].splice(ptr.index).filter((token) => token.type != "Space"));
+    this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index], { name: "SyntaxError", message: "Wrong For loop declaration", ptr }, "Variable");
 
-  //   let { value } = this.tokens[this.line][this.index++];
-  //   this.stateChecker("type", this.tokens[this.line][this.index++], "Wrong For loop declaration", "IN Keyword");
-  //   this.stateChecker("type", this.tokens[this.line][this.index++], "Wrong For loop declaration", "Variable");
+    let { value } = ptr.tokens[ptr.line][ptr.index++];
+    this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Wrong For loop declaration", ptr }, "IN Keyword");
+    this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Wrong For loop declaration", ptr }, "Variable");
 
-  //   let range = this.parseVariable();
+    let range = this.parseVariable(ptr);
 
-  //   // Check on LIST type
-  //   this.stateChecker("type", range.defined, `${range.defined.type} Object is Not Iterable`, "LIST");
-  //   this.stateChecker("type", this.tokens[this.line][this.index++], "Wrong For loop declaration", "Start Block");
+    // Check on LIST type
+    this.err.checkObj("type", range.defined, { name: "TypeError", message: `${range.defined?.type ?? "Undeclared"} Object is Not Iterable`, ptr }, "LIST");
+    this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Wrong For loop declaration", ptr }, "Start Block");
 
-  //   // Create a {{value}} as a temporary variable that contain nothing
-  //   this.currLevel.header.push({ Statement: { type: "VAR", name: `_${value}`, defined: range.defined.defined } });
-  //   return { type: "FOR", iter: `_${value}`, range: range };
-  // }
+    // Create a {{value}} as a temporary variable that contain nothing
+    ptr.currLevel.header.push({ Statement: { type: "VAR", name: `_${value}`, defined: (range.defined as List)?.defined } });
+    return { type: "FOR", iter: `_${value}`, range: range, body: [] as Operation[] };
+  }
 
-  // function parseReturn() {
-  //   // Delete all spaces
-  //   this.tokens[this.line].push(...this.tokens[this.line].splice(this.index).filter((token) => token.type != "Space"));
+  parseReturn(ptr: Parser): Return {
+    // Delete all spaces
+    ptr.tokens[ptr.line].push(...ptr.tokens[ptr.line].splice(ptr.index).filter((token) => token.type != "Space"));
 
-  //   // Check the pass keyword, let's check if user put some variable after, if so throw an Error
-  //   if (this.tokens[this.line][this.index - 1].type.includes("Pass") && this.tokens[this.line][this.index])
-  //     this.errorMessageHandler("Invalid syntax", this.tokens[this.line][this.index - 1]);
+    // Check the pass keyword, let's check if user put some variable after, if so throw an Error
+    if (ptr.tokens[ptr.line][ptr.index - 1].type.includes("Pass") && ptr.tokens[ptr.line][ptr.index])
+      this.err.message({ name: "SyntaxError", message: `Invalid syntax. Can not return value with "pass"`, ptr });
 
-  //   // Check if the function return any of the type, if not then put as a return value '0'
-  //   let { type } = this.tokens[this.line][this.index] || { type: "" };
-  //   if (!this.isInclude(type, "Variable", "Number", "Char", "String", "Unary", "Parentheses"))
-  //     return { type: "RET", Expression: { value: "0", type: "INT", kind: 10 }, defined: { type: "INT", kind: 10 } };
-  //   return { type: "RET", Expression: this.parseExpression({}), defined: this.type.curr };
-  // }
+    // Check if the function return any of the type, if not then put as a return value '0'
+    let { type } = ptr.tokens[ptr.line][ptr.index] || { type: "" };
+    if (!isInclude(type, "Variable", "Number", "Char", "String", "Unary", "Parentheses")) return { type: "RET", defined: { value: "", type: "NONE" } };
+    return { type: "RET", Expression: this.exp.parse(ptr), defined: this.exp.type.curr };
+  }
 
   parseVariable(ptr: Parser): Assign | FuncCall {
     // Delete all spaces
@@ -174,8 +184,7 @@ class Statement {
     );
 
     if (ptr.tokens[ptr.line][ptr.index].type.includes("Assignment")) return this.parseVariableAssign(ptr);
-    // return this.parseFuncCaller();
-    return {} as FuncCall;
+    return this.parseFuncCaller(ptr);
   }
 
   private parseVariableAssign(ptr: Parser): Assign {
@@ -198,57 +207,73 @@ class Statement {
     return { type: "VAR", name: `_${value}`, Expression: this.exp.parse(ptr), defined: this.exp.type.curr };
   }
 
-  // //
-  // // TODO: TO rebuild type of function's body depends on input value, if it's allowed
-  // //          For this You need to add another variable which will link to the final
-  // //          Object
-  // //
-  // function getArgs(params) {
-  //   let args = [];
-  //   // TODO: Change ast copy from JSON to copyTree ...
-  //   let prevState = { type: JSON.parse(JSON.stringify(this.type)), ast: this.ast && JSON.parse(JSON.stringify(this.ast)), parentheses: this.parentheses };
-  //   this.stateChecker("type", this.tokens[this.line][this.index - 1], "Wrong Function declaration Syntax", "Open Parentheses");
+  //
+  // TODO: TO rebuild type of function's body depends on input value, if it's allowed
+  //          For this You need to add another variable which will link to the final
+  //          Object
+  //
+  private getArgs(ptr: Parser, params: Assign[]): Assign[] {
+    let args: Assign[] = [];
+    // TODO: Change ast copy from JSON to copyTree ...
+    let prevState = {
+      type: JSON.parse(JSON.stringify(this.exp.type)),
+      ast: JSON.parse(JSON.stringify(this.exp.ast)),
+      parentheses: this.exp.parentheses,
+    };
 
-  //   for (let param of params) {
-  //     let type = param.defined.type == "ANY" ? ["INT", "VAR", "STR", "FLOAT", "ANY"] : [param.defined.type];
-  //     this.type = { prev: {}, curr: {} };
-  //     this.ast = undefined;
-  //     this.parentheses = 0;
+    this.err.checkObj(
+      "type",
+      ptr.tokens[ptr.line][ptr.index - 1],
+      { name: "SyntaxError", message: "Wrong Function declaration Syntax", ptr },
+      "Open Parentheses"
+    );
 
-  //     args.push(this.parseExpression({}));
-  //     let curr = this.type.curr.type == "INT" && type[0] == "FLOAT" ? { type: "FLOAT" } : this.type.curr;
-  //     this.stateChecker("type", curr, "Wrong arguments declaration", ...type);
-  //     // TODO: Somehow update the final body
-  //     // if (param.defined.type == "ANY") param.defined = this.type.curr;
+    for (let param of params) {
+      let type = param.defined.type == "ANY" ? ["INT", "VAR", "STR", "FLOAT", "ANY"] : [param.defined.type];
 
-  //     args.push({ type: "NONE", Expression: args.pop(), defined: curr });
+      let argv = this.exp.parse(ptr);
+      let curr = this.exp.type.curr.type == "INT" && type[0] == "FLOAT" ? { value: "", type: "FLOAT" } : this.exp.type.curr;
+      this.err.checkObj("type", curr, { name: "SyntaxError", message: "Wrong arguments declaration", ptr }, ...type);
+      // TODO: Somehow update the final body
+      // if (param.defined.type == "ANY") param.defined = this.type.curr;
 
-  //     // Check next step if it Close Parentheses then exit from the loop
-  //     // Else check if the next token is comma
-  //     if ((this.tokens[this.line][this.index] || {}).type == "Close Parentheses") break;
-  //     this.stateChecker("type", this.tokens[this.line][this.index++], "Wrong Function declaration Syntax", "Comma");
-  //   }
+      args.push({ type: "NONE", name: "", Expression: argv, defined: curr });
 
-  //   // Check on Closing Parentheses and restore previous State
-  //   this.stateChecker("type", this.tokens[this.line][this.index++], "Close Parentheses are missing", "Close Parentheses");
+      // Check next step if it Close Parentheses then exit from the loop
+      // Else check if the next token is comma
+      if (ptr.tokens[ptr.line][ptr.index]?.type == "Close Parentheses") break;
+      this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Wrong Function declaration Syntax", ptr }, "Comma");
+    }
 
-  //   this.type = prevState.type;
-  //   this.ast = prevState.ast;
-  //   this.parentheses = prevState.parentheses;
+    // Check on Closing Parentheses and restore previous State
+    this.err.checkObj("type", ptr.tokens[ptr.line][ptr.index++], { name: "SyntaxError", message: "Close Parentheses are missing", ptr }, "Close Parentheses");
 
-  //   return args;
-  // }
+    this.exp.type = prevState.type;
+    this.exp.ast = prevState.ast;
+    this.exp.parentheses = prevState.parentheses;
 
-  // function parseFuncCaller() {
-  //   let { value } = this.tokens[this.line][this.index++ - 1];
+    return args;
+  }
 
-  //   // TODO: To improve function name, they should contain a number at the end
-  //   // which will tell the amount of params/args
+  private parseFuncCaller(ptr: Parser): FuncCall {
+    let { value } = ptr.tokens[ptr.line][ptr.index++ - 1];
 
-  //   let { params, defined, basic, require = [] } = this.checkOnBasicFunc(value) || this.getDefinedToken("Declaration", "name", `_${value}`, this.currLevel);
-  //   let args = this.getArgs(params);
-  //   return { type: "FUNC_CALL", name: basic ? `${value.toUpperCase()}${args.length}` : `_${value}`, params: [...args, ...require], defined: defined };
-  // }
+    // TODO: To improve function name, they should contain a number at the end
+    // which will tell the amount of params/args
+
+    type funcProperty = { params: Assign[]; defined: Types; basic?: any[]; require?: any[] };
+    // let { params, defined, basic, require = [] } = this.checkOnBasicFunc(value) || this.getDefinedToken("Declaration", "name", `_${value}`, this.currLevel);
+    let { params, defined, basic = [], require = [] }: funcProperty =
+      (getDefinedToken("Declaration", "name", `_${value}`, ptr.currLevel) as Declaration) ?? ({} as Declaration);
+    let args = this.getArgs(ptr, params);
+
+    return {
+      type: "FUNC_CALL",
+      name: basic ? `${value.toUpperCase()}${args.length}` : `_${value}`,
+      params: [...args, ...require],
+      defined: defined,
+    } as FuncCall;
+  }
 }
 
 export default Statement;
