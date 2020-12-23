19174e0e7475d43e468433196ceda068ef7de4dc
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-12-24 01:41:34 +0200

    Add some changes

--- SourceCode/Test.py
+++ SourceCode/Test.py
@@ -1,16 +1,23 @@
 # Test
 
 def main(a):
-    while (1):
-        b = -(1 ** -2) ** a
-        break
+    def test():
+        return 1
+#     while (1):
+#         b = -(1 ** -2) ** a
+#         break
 
     return "5"
 
 c = 1
 
 main(1)
-a = [1 * 2 - 1 * 3 + 5, 2, 3 + 2]
 
-for i in a:
-    print(0)
+if c:
+    a = 1
+
+b = 3
+# a = [1 * 2 - 1 * 3 + 5, 2, 3 + 2]
+
+# for i in a:
+#     print(0)
---
+++ SourceCode/Test.ts
@@ -0,0 +1,15 @@
+function main(a: any) {
+  function test() {
+    return 1;
+  }
+
+  return 5;
+}
+
+let c = 1;
+console.log(main(1));
+if (c) {
+  let a = 1;
+}
+
+let b = 3;
