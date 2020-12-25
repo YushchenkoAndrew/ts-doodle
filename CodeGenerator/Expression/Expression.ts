85badd3e463f4b4508b991517bbb5e257db4e079
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-12-25 02:24:28 +0200

    Add UnaryOperations

--- CodeGenerator/Expression/Expression.ts
+++ CodeGenerator/Expression/Expression.ts
@@ -1,4 +1,4 @@
-import { Int, Types, Str, Var, Float, BinaryOperation, List } from "../../Parser/Expression/Interfaces";
+import { Int, Types, Str, Var, Float, BinaryOperation, List, UnaryOperation } from "../../Parser/Expression/Interfaces";
 import { isInclude } from "../../lib/index";
 import { FuncCall } from "../../Parser/Statement/Interfaces";
 import commands from "./Commands.json";
@@ -9,10 +9,10 @@ class Expression {
 
     switch (type) {
       case "Binary Operation":
-        return this.parseOperation(tree as BinaryOperation);
+        return this.parseBinOperation(tree as BinaryOperation);
 
       case "Unary Operation":
-        return "";
+        return this.parserUnaryOperation(tree as UnaryOperation);
 
       case "FUNC_CALL":
         // TODO: Allow to set the specific arg to value (allow Assign)
@@ -31,8 +31,16 @@ class Expression {
     }
   }
 
-  private parseOperation({ value, left, right = {} as Types }: BinaryOperation) {
+  private parseBinOperation({ value, left, right = {} as Types }: BinaryOperation) {
     if (!isInclude(value, ...Object.keys(commands))) return `${this.parse(left)} ${value} ${this.parse(right)}`;
+
+    // TODO:
+    // Some other BinaryOperations that different and depends strictly on language
+    return "";
+  }
+
+  private parserUnaryOperation({ value, exp }: UnaryOperation) {
+    if (!isInclude(value, ...Object.keys(commands))) return `${value}${this.parse(exp)}`;
 
     // TODO:
     // Some other BinaryOperations that different and depends strictly on language
