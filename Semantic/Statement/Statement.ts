add5f26dc36323a3909415fa344d28e7626322b7
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-01-04 01:30:52 +0200

    Create simple Syntax transformer of creating Ternary Operations

--- Semantic/Statement/Statement.ts
+++ Semantic/Statement/Statement.ts
@@ -1,6 +1,9 @@
+import * as dotenv from "dotenv";
 import { OperationTypes, Operation } from "../../Parser/Interfaces";
 import { findOperations } from "../../lib/";
-import { Assign, Condition } from "../../Parser/Statement/Interfaces";
+import { Assign, Condition, Return } from "../../Parser/Statement/Interfaces";
+import { Types } from "../../Parser/Expression/Interfaces";
+dotenv.config();
 
 class Statement {
   parse(curr: OperationTypes, body: Operation[]) {
@@ -19,13 +22,15 @@ class Statement {
     // Check if new Variable is init assign and it have different types then
     // add them to the init assign
     if (init.init) {
-      console.log(`VAR INIT "${name}"`);
-
+      // TODO: Create a depth finder for example => a = 5; if a: a = 1
+      // value 'a' shouldn't be 'const' as it's now defined
       let types = (findOperations("Statement", "name", name, body.slice(1)) as Assign[]).map((item) => item.defined[0]);
 
       // Check if this variable has some new assignments
       if (types.length) defined.push(...types.filter(({ type }) => type != defined[0].type));
       else init.type = "CONST";
+
+      if (process.env.DEBUG) console.log(`DEFINE TO VAR "${name}" TYPES [${defined.map((item) => item.type)}]`);
     }
   }
 
@@ -33,12 +38,52 @@ class Statement {
     let bodySize = curr.body.length;
     let elseSize = curr.else?.length ?? 0;
 
-    if (bodySize * elseSize == 1) {
-      // console.log();
-      console.log(curr.body[0].Statement);
-      console.log(body);
+    if (bodySize * elseSize != 1 || curr.body[0].Statement?.type != curr.else?.[0]?.Statement.type) return;
+    let { type } = curr.body[0].Statement;
 
-      body[0].Statement = curr.body[0].Statement;
+    switch (type) {
+      case "VAR": {
+        // Save "then" and "else" body values
+        let ternary = { then: curr.body[0].Statement as Assign, else: curr.else[0].Statement as Assign };
+        if (ternary.then.name != ternary.else.name) break;
+
+        // Copy to the Statement Assign Operation from then body
+        body[0].Statement = ternary.then;
+
+        // Set "then" and "else" body to the appropriate Expression
+        (body[0].Statement as Assign).Expression = {
+          ...curr,
+          body: [{ Expression: ternary.then.Expression as Types }],
+          else: [{ Expression: ternary.else.Expression as Types }],
+        };
+
+        if (process.env.DEBUG) {
+          console.log("CHANGED CONDITION TO:");
+          console.dir(body[0], { depth: 3 });
+        }
+        break;
+      }
+
+      case "RET": {
+        // Save "then" and "else" body values
+        let ternary = { then: curr.body[0].Statement as Return, else: curr.else[0].Statement as Return };
+
+        // Copy to the Statement Assign Operation from then body
+        body[0].Statement = ternary.then;
+
+        // Set "then" and "else" body to the appropriate Expression
+        (body[0].Statement as Assign).Expression = {
+          ...curr,
+          body: [{ Expression: ternary.then.Expression as Types }],
+          else: [{ Expression: ternary.else.Expression as Types }],
+        };
+
+        if (process.env.DEBUG) {
+          console.log("CHANGED RETURN TO:");
+          console.dir(body[0], { depth: 3 });
+        }
+        break;
+      }
     }
   }
 }
